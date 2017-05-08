603aae337a8d080b9c55a1b2e890f6f4526bc65a
ranjitwarwick <ranjit10@hotmail.com>
2017-05-08 10:07:16 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+
+
+?>
+
+<html>
+ <head> 
+  <link href="mayhome.css" rel="stylesheet" type="text/css">
+  <script src="mayhome.js" type="text/javascript"></script>  
+ <head> 
+ <body onload="setupPage">
+ 
+ <div id="wrapper">
+  <div id="titleOfPage">Ranjit Samrai</div>
+  
+ 
+ <div id="linksToWebsites">
+  <div id="linkToPhotosDiv">
+       <button id="button1" onclick="openNewWindow('http://photogalleryranjit.azurewebsites.net')" type="button">Photo Gallery</button>
+       Opens a new window 
+  </div>
+  <div  id="linkToDartsDiv">
+      <button  id="button2" onclick="openNewWindow('http://dartsscore.azurewebsites.net')" type="button">Darts Scoreboard</button>
+      Opens a new window
+  </div>
+  </div> <!--End Div linksToWebsites -->
+
+  <div id ="versionInfo">Last updated: Saturday 6th May 2017</div>
+ </div> <!--End Div of wrapper -->
+ </body>
+</html>
+
---
+++ mayhome.js
@@ -0,0 +1,9 @@
+function setupPage()
+{
+
+}
+
+function openNewWindow(urlName)
+{
+  window.open(urlName, '_blank', 'location=yes,height=900,width=1600,scrollbars=yes,status=yes');
+}
